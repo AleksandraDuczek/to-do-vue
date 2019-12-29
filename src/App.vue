6c91d8ba0bd8d9e6ee42bf421944ee552ddaddf7
Aleksandra Duczek <oladuczek231@gmail.com>
2019-12-29 14:31:20 +0100

    add some cuts, add delete function

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,51 @@
 <template>
   <div id="app">
-    <div class="bg-primary text-white text-center p-2 mt-2">
+    <div class="bg-info text-white text-center p-2 mt-2">
       Lista zadań użytkownika {{name}}
     </div>
+
     <div class="container-fluid p-4">
+      <div class="row" v-if="!filteredTasks.length">
+        <div class="col text-center">
+          <div>Brak zadań do wykonania</div>
+          <span class="mdi mdi-emoticon-happy"></span>
+        </div>
+      </div>
+
       <div class="row">
         <div class="col font-weight-bold">Zadanie</div>
         <div class="col-2 font-weight-bold">Zakończono?</div>
       </div>
+
       <div class="row" v-for="task in filteredTasks" :key="task.action">
         <div class="col">{{task.action}}</div>
         <div class="col-2 text-center">
           <input v-model="task.done" type="checkbox" class="form-check-input"/>
       </div>
     </div>
+
       <div class="row py-2">
         <div class="col">
           <input v-on:keyup.enter="addNewTodo" v-model="newItemText" class="form-control"/>
         </div>
         <div class="col-2">
-          <button v-on:click="addNewTodo" class="btn btn-primary">
+          <button v-on:click="addNewTodo" class="btn btn-info">
             Dodaj
           </button>
         </div>
       </div>
-      <div class="row bg-secondary py-2 mt-2 text-white">
+      <div class="row bg-dark py-2 mt-4 text-white">
         <div class="col text-center">
           <input v-model="hideCompleted" type="checkbox" class="form-check-input" />
           <label class="form-check-label font-weight-bold">
             Ukryj zakończone zadania
           </label>
+        </div>
+        <div class="col text-center">
+          <button class="btn btn-sm btn-danger"
+                  v-on:click="deleteCompleted">
+            Usuń zakończone
+          </button>
         </div>
       </div>
   </div>
@@ -63,6 +79,13 @@ export default {
         localStorage.setItem("todos", JSON.stringify(this.tasks));
         this.newItemText = '';
     },
+     storeData() {
+        localStorage.setItem("todos", JSON.stringify(this.tasks));
+     },
+      deleteCompleted() {
+        this.tasks = this.tasks.filter(task => !task.done);
+        this.storeData();
+      }
   },
   created() {
       let data = localStorage.getItem("todos");
@@ -76,10 +99,5 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-}
-.container-fluid {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
 }
 </style>
